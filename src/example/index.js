ebd3892a9fbc276037ac623e03bfa85adfa2f109
Xavier Via <fernando.via@gmail.com>
2017-05-30 16:23:58 +0200

    Add support for put all passed props in a single context prop

--- src/example/index.js
+++ src/example/index.js
@@ -3,26 +3,44 @@ import PropTypes from 'prop-types'
 import { render } from 'react-dom'
 import { getContextualizer, withPropsFromContext } from '../'
 
-function Text ({ label, color }) {
+function Text({ label, color }) {
   return <p style={{ color }}>{label}</p>
+}
+
+function TextThatUsesTheme({ label, theme }) {
+  return <p style={{ color: theme.mainColor }}>{label}</p>
 }
 
 const Contextualizer = getContextualizer({ color: PropTypes.string })
 const TextThatGetsColorFromContext = withPropsFromContext(['color'])(Text)
+const ContextualizeToProp = getContextualizer({ mainColor: PropTypes.string }, 'theme')
+const TextThatUsesThemeAndGetsColorFromContext = withPropsFromContext(['theme'])(TextThatUsesTheme)
 
 render(
   <div>
     <h2>All text here is red</h2>
-    <Contextualizer color='red'>
-      <TextThatGetsColorFromContext label='Lorem ipsum' />
-      <TextThatGetsColorFromContext label='Dolor sit amet' />
+    <Contextualizer color="red">
+      <TextThatGetsColorFromContext label="Lorem ipsum" />
+      <TextThatGetsColorFromContext label="Dolor sit amet" />
     </Contextualizer>
 
     <h2>All text here is green</h2>
-    <Contextualizer color='green'>
-      <TextThatGetsColorFromContext label='Lorem ipsum' />
-      <TextThatGetsColorFromContext label='Dolor sit amet' />
+    <Contextualizer color="green">
+      <TextThatGetsColorFromContext label="Lorem ipsum" />
+      <TextThatGetsColorFromContext label="Dolor sit amet" />
     </Contextualizer>
-  </div>
-  , document.getElementById('react-context-props')
+
+    <h2>All text here is blue</h2>
+    <ContextualizeToProp mainColor="blue">
+      <TextThatUsesThemeAndGetsColorFromContext label="Lorem ipsum" />
+      <TextThatUsesThemeAndGetsColorFromContext label="Dolor sit amet" />
+    </ContextualizeToProp>
+
+    <h2>All text here is yellow</h2>
+    <ContextualizeToProp mainColor="yellow">
+      <TextThatUsesThemeAndGetsColorFromContext label="Lorem ipsum" />
+      <TextThatUsesThemeAndGetsColorFromContext label="Dolor sit amet" />
+    </ContextualizeToProp>
+  </div>,
+  document.getElementById('react-context-props')
 )
--- src/index.js
+++ src/index.js
@@ -1,60 +1,53 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-export const getContextualizer = propTypes => {
+export const getContextualizer = (propTypes, targetProp) => {
   class ContextProps extends Component {
-    getChildContext () {
-      const props = Object.keys(this.props)
-        .reduce((x, key) => {
-          if (key !== 'children') {
-            x[key] = this.props[key]
-          }
+    getChildContext() {
+      const props = Object.keys(this.props).reduce((x, key) => {
+        if (key !== 'children') {
+          x[key] = this.props[key]
+        }
 
-          return x
-        }, {})
+        return x
+      }, {})
 
-      return props
+      return targetProp ? { [targetProp]: props } : props
     }
 
-    render () {
+    render() {
       return <span>{this.props.children}</span>
     }
   }
 
   ContextProps.displayName = 'ContextProps'
 
-  ContextProps.childContextTypes = propTypes
+  ContextProps.childContextTypes = targetProp
+    ? { [targetProp]: PropTypes.shape(propTypes) }
+    : propTypes
   return ContextProps
 }
 
 export const withPropsFromContext = propList => Target => {
   class WithPropsFromContext extends Component {
-    render () {
+    render() {
       const props = {
-        ...propList.reduce(
-          (x, prop) => {
-            x[prop] = this.context[prop]
+        ...propList.reduce((x, prop) => {
+          x[prop] = this.context[prop]
 
-            return x
-          },
-          {}
-        ),
-        ...this.props
+          return x
+        }, {}),
+        ...this.props,
       }
 
-      return (
-        <Target {...props} />
-      )
+      return <Target {...props} />
     }
   }
 
-  WithPropsFromContext.contextTypes = propList.reduce(
-    (x, prop) => {
-      x[prop] = PropTypes.any
-      return x
-    },
-    {}
-  )
+  WithPropsFromContext.contextTypes = propList.reduce((x, prop) => {
+    x[prop] = PropTypes.any
+    return x
+  }, {})
 
   WithPropsFromContext.displayName = Target.displayName || Target.name
 
